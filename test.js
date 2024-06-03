0f9c569e838178469fa3879485ecbcd1017d2b46
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-06-03 21:51:57 +1000

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('Blog Application', () => {
+  it('should return a 200 status code for the home page', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  // Add more test cases for your application
+});
